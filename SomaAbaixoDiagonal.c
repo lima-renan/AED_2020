66fdaf2f3bc11225a5b9886e6961d17ebb37012c
lima-renan <renan.flimabr@gmail.com>
2020-02-16 19:34:31 -0300

    aula 1

--- SomaAbaixoDiagonal.c
+++ SomaAbaixoDiagonal.c
@@ -1,13 +1,13 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int SomaAbaixoDiagonal (int *m){
-    int sum = 0;
+int SomaAbaixoDiagonal (int **m, int n){
+    int sum = 0, *p;
   for(int i = 0; i<n; i++){
     for(int k = 0; k<n; k++){
         if(i == k && i != (n-1) && k != (n-1) ){
-            for(l = 1; (i+l) < n; l++){
-                sum = sum = sum + m[(i+l)][k];
+            for(int l = 1; (i+l) < n; l++){
+                sum = sum + m[(i+l)][k];
             }
         }
     }
@@ -16,14 +16,17 @@ int SomaAbaixoDiagonal (int *m){
 }
 
 int main() {
-  int *m, sum;
-  m = (int *)malloc(n * sizeof(int));
-    for (i = 0; i < n; i++) {
+  int **m, n, sum;
+  scanf("%d",&n);
+  m = (int **)malloc(n * sizeof(int**));
+    for (int i = 0; i < n; i++) {
+      m[i] = (int *)malloc(n * sizeof(int*));
         for(int k = 0; k<n; k++){
            scanf("%d",&m[i][k]);
         }
     }
-  sum = SomaAbaixoDiagonal(m) ;
+  sum = SomaAbaixoDiagonal(m, n) ;
   printf("%d\n", sum);
+  free(m);
   return 0;
 }

64a474d44c6c3c1d64b7624b3e55466b1faca702
lima-renan <renan.flimabr@gmail.com>
2020-03-02 22:30:10 -0300

    aula 2

--- Aula 2/ListaAlunosMedidas.c
+++ Aula 2/ListaAlunosMedidas.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <math.h>
 
 typedef struct aluno {
   int ra;
@@ -17,33 +18,38 @@ typedef struct medidas {
 
 void ImprimeLista(aluno *a, int n){
   int apr = 0, rep = 0;
-  float media;
+  float *media;
   char result[20];
+  medidas sala;
+  sala.media = 0;
+  sala.variancia = 0;
+  media = (float *)(malloc(n*sizeof(float)));
   for(int i = 0; i < n; i++){
-    media = ((a[i].p1 + a[i].p2)*0.35)+ (a[i].atv * 0.3);
-    if(media < 5.0){
+    media[i] = ((a[i].p1 + a[i].p2)*0.35)+ (a[i].atv * 0.3);
+    sala.media = sala.media + media[i];
+    if(media[i] < 5.0){
       rep ++;
-       printf("%d %s %.2f Reprovado\n", a[i].ra, a[i].nome, media);
+       printf("%d %s %.2f Reprovado\n", a[i].ra, a[i].nome, media[i]);
     }
     else{
       apr ++;
-      printf("%d %s %.2f Aprovado\n", a[i].ra, a[i].nome, media);
+      printf("%d %s %.2f Aprovado\n", a[i].ra, a[i].nome, media[i]);
     }
   }
-   printf("Aprovados: %d\n", apr);
-   printf("Reprovados: %d\n", rep);
+  sala.media = sala.media/n;
+  for(int i = 0; i < n; i++){
+    sala.variancia = sala.variancia + pow((media[i]-sala.media),2);
+  }
+  sala.variancia = sala.variancia/n;
+  sala.desvio_padrao = pow(sala.variancia,0.5);
+  printf("Aprovados: %d\n", apr);
+  printf("Reprovados: %d\n", rep);
+  printf("Media: %.2f\n", sala.media);
+  printf("Variancia: %.2f\n", sala.variancia);
+  printf("Desvio padrao: %.2f\n", sala.desvio_padrao);
+  free(media);
 }
 
-medidas Medias (aluno *a, int n){
-  int apr = 0, rep = 0;
-  float media = 0;
-  char result[20];
-  for(int i = 0; i < n; i++){
-    media = media + ((a[i].p1 + a[i].p2)*0.35)+ (a[i].atv * 0.3);
-  
-  }
-  media = media/n;
-}
 
 int main(void) {
   int n;
@@ -54,5 +60,6 @@ int main(void) {
     scanf("%d %s %f %f %f", &a[i].ra,a[i].nome, &a[i].p1, &a[i].p2, &a[i].atv);
   }
   ImprimeLista(a, n);
+  free(a);
   return 0;
 }
